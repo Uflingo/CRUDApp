1f20ee74a815fba1f6fab922691d23ea68bd0e19
uflingo <ufo_2@mail.ru>
2017-07-03 03:34:55 +0300

    updated app

--- src/main/java/org/uflingo/controller/UserController.java
+++ src/main/java/org/uflingo/controller/UserController.java
@@ -3,64 +3,80 @@ package org.uflingo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.HttpServletBean;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.uflingo.model.User;
 import org.uflingo.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 public class UserController {
-
-    @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/")
-    public ModelAndView listUser(ModelAndView model){
-        List<User> listUser = userService.getAllUsers();
-        model.addObject("listUser",listUser);
-        model.setViewName("home");
-        return model;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
-    @RequestMapping(value = "/newUser", method = RequestMethod.GET)
-    public ModelAndView newUser(ModelAndView model){
-        User user = new User();
-        model.addObject("user", user);
-        model.setViewName("UserForm");
-        return model;
+    @RequestMapping(value = "users", method = RequestMethod.GET)
+    public String listUsers(@RequestParam(value="page", required = false) Long page, Model model) {
+        if (null == page)
+            page = 1L;
+        model.addAttribute("user", new User());
+        model.addAttribute("searcheduser", new User());
+        model.addAttribute("listUsers", userService.getUsers(page));
+        model.addAttribute("page", page);
+
+        return "users";
     }
 
-    @RequestMapping(value = "/saveUser", method = RequestMethod.POST)
-    public ModelAndView saveUser(@ModelAttribute User user){
-        if (user.getId() == 0)
-            userService.addUser(user);
-        else
-            userService.updateUser(user);
-        return new ModelAndView("redirect:/");
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+    @RequestMapping(value = "/users/edit", method = RequestMethod.POST)
+    public String editUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
     }
 
-    @RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
-    public ModelAndView deleteUser(HttpServletRequest request){
-        int userId = Integer.parseInt(request.getParameter("id"));
-        userService.deleteUser(userId);
-        return new ModelAndView("redirect:/");
+    @RequestMapping("/remove/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+
+        return "redirect:/users";
     }
 
-    @RequestMapping(value = "/editUser", method = RequestMethod.GET)
-    public ModelAndView editUser(HttpServletRequest request){
-        int userId = Integer.parseInt(request.getParameter("id"));
-        User user = userService.getUser(userId);
-        ModelAndView model = new ModelAndView("UserForm");
-        model.addObject("user",user);
-        return model;
+    @RequestMapping("edit/{id}")
+    public String editUser(@PathVariable("id") int id, @RequestParam(value="page", required = false) Long page, Model model) {
+        if (null == page) page = 1L;
+        model.addAttribute("user", userService.getUser(id));
+        model.addAttribute("searcheduser", new User());
+        model.addAttribute("listUsers", userService.getUsers(page));
+        model.addAttribute("page", page);
+
+        return "edituser";
     }
 
+    @RequestMapping("userdata/{id}")
+    public String userData(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
 
+        return "userdata";
+    }
 
+    @RequestMapping("adduser")
+    public String createUser(@ModelAttribute User user) {
+        return "adduser";
+    }
+
+    @RequestMapping(value="searchresults", method = RequestMethod.POST)
+    public String searchResults(@ModelAttribute("searcheduser") User user, Model model) {
+        List<User> searchResult = userService.getUsers(user.getName());
+        model.addAttribute("listUsers", searchResult);
+
+        return "searchresults";
+    }
 }
